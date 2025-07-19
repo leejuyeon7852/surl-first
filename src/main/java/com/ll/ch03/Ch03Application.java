c0ba127b886c47d973f71b33d38a1bb3dfe32959
zuyo7852 <leejuyeon7852@gmail.com>
2025-07-19 15:00:12 +0900

    work

---
+++ src/main/java/com/ll/ch03/Ch03Application.java
@@ -0,0 +1,13 @@
+package com.ll.ch03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch03Application.class, args);
+	}
+
+}
